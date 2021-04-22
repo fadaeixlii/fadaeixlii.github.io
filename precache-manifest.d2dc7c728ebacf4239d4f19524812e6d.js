18962754a79d58b1b6f5ce39dc75b94698351583
MohammaD MKH <mohammad4252@gmail.com>
2021-04-22 10:57:49 +0430

    final 3

---
+++ precache-manifest.d2dc7c728ebacf4239d4f19524812e6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2789c6beb56ccc0efa7200da8108cb53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "729f40fdeeff97b0f66b",
+    "url": "/static/css/main.97ca423f.chunk.css"
+  },
+  {
+    "revision": "df96afbb52e1d51c6da4",
+    "url": "/static/js/2.3ce171be.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.3ce171be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729f40fdeeff97b0f66b",
+    "url": "/static/js/main.dbfd20dc.chunk.js"
+  },
+  {
+    "revision": "ddb23d41b025de166703",
+    "url": "/static/js/runtime-main.60ded422.js"
+  },
+  {
+    "revision": "908c2ae171c304cf28dd3a675225b898",
+    "url": "/static/media/mohammadKhani.908c2ae1.jpg"
+  }
+]);
